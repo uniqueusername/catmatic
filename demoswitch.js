0997c488355f5d768eaea6ea4a0f02617967f44c
Aryan Jha <aryan@jha.io>
2017-04-10 22:07:35 -0700

    Created demos page

---
+++ demoswitch.js
@@ -0,0 +1,7 @@
+function setMenu() {
+  document.getElementById('scplaylist').src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/314300565%3Fsecret_token%3Ds-CnfVM&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true";
+}
+
+function setActive() {
+  document.getElementById('scplaylist').src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/314623890%3Fsecret_token%3Ds-9SR81&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true";
+}
